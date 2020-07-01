549ab4bd0371d5575890c2c99e2bbf8b7dbb1264
Luís <67702059+LuisCMoraes@users.noreply.github.com>
2020-07-01 15:06:32 -0300

    Update Pt4_Ex02.java

--- Pt4_Ex02.java
+++ Pt4_Ex02.java
@@ -12,19 +12,19 @@ class Pt4_Ex02{
         int m[][] = new int[2][4];
         //Variáveis:
         int l, c, cont = 0, par = 0;
-        float med = 0;
+      
         String mat = "";
         String frase = "Qtd de elementos entre 12 e 20:";
         //Código:
         for(l=0;l<2;l++){
             cont = 0;
             for(c=0;c<4;c++){
-                m[l][c] = Integer.parseInt(JOptionPane.showInputDialog("Preencha a matriz:\n" + "Linha: " + (l+1) + "\nColuna: " + (c+1)));
+                
                 if(m[l][c] > 12 && m[l][c] < 20){
                     cont++;
                 }
                 if(m[l][c] %2 == 0){
-                    par++;
+                    
                     med = med + m[l][c];
                 }
                 //String para exibir a matriz no final:
@@ -37,6 +37,6 @@ class Pt4_Ex02{
         med = med / par;
         frase = frase + "\n\nMédia dos elementos pares da matriz:\n" + med +
                 "\n\nMatriz preenchida:\n" + mat;
-        JOptionPane.showMessageDialog(null, frase);
+      
     }
 }
